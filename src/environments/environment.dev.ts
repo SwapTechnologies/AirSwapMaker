0f6b26627104fd203c9e75765fd820142f1587ca
DomiDre <homiedomi@gmail.com>
2018-07-24 00:43:41 +0200

    Set environment files for dev and prod

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,5 +5,10 @@
 
 export const AppConfig = {
   production: false,
-  environment: 'DEV'
+  environment: 'DEV',
+  networkId: 'mainnet',
+  tokenMetadata: 'https://token-metadata.airswap.io/tokens',
+  astAddress: '0x27054b13b1b798b345b591a4d22e6562d47ea75a',
+  wethAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  ethAddress: '0x0000000000000000000000000000000000000000',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const AppConfig = {
   production: true,
-  environment: 'PROD'
+  environment: 'PROD',
+  networkId: 'mainnet',
+  tokenMetadata: 'https://token-metadata.airswap.io/tokens',
+  astAddress: '0x27054b13b1b798b345b591a4d22e6562d47ea75a',
+  wethAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  ethAddress: '0x0000000000000000000000000000000000000000',
 };
